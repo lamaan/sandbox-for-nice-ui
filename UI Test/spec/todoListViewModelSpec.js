4a435622fdb35b191a80c11190045c668ae43c34
lamaan <lamaan@lamaan.com>
2013-11-20 08:14:45 +0000

    basic solution added

---
+++ UI Test/spec/todoListViewModelSpec.js
@@ -0,0 +1,178 @@
+describe("when there are no todos", function() {
+    
+	var todoList = new TodoListViewModel();
+	it("a way to add a task should be the only component present", function() {
+		
+        expect(todoList.todos().length).toEqual(0);
+    });
+});
+describe("when adding a new todo", function(){
+
+	var todoList = new TodoListViewModel();	
+	todoList.currentTask().name("test");
+	describe(" and enter is clicked",function(){
+		todoList.saveTask();
+		it("should append the task to the todo list", function(){
+			expect(todoList.todos().length).toEqual(1);
+		});
+		
+	});
+	describe("which has trailing spaces", function(){
+		var todoList = new TodoListViewModel();	
+		todoList.currentTask().name("test   ");
+		describe(" and enter is clicked",function(){
+			todoList.saveTask();
+			it("should append the task to the todo list", function(){
+				expect(todoList.todos().length).toEqual(1);
+			});
+			it("should remove the white space from the task", function(){
+				expect(todoList.todos()[0].name()).toEqual("test");
+			});
+			
+			
+		});
+	});
+	describe("which has only white space text", function(){
+		var todoList = new TodoListViewModel();	
+		todoList.currentTask().name("   ");
+		describe(" and enter is clicked",function(){
+			todoList.saveTask();
+			it("should not append the task to the todo list", function(){
+				expect(todoList.todos().length).toEqual(0);
+			});			
+		});
+	});
+});
+
+describe("when an incomplete todo is in the list", function(){
+	var todoList = new TodoListViewModel();	
+	todoList.todos().push(new Task());
+	todoList.todos()[0].name("some task");
+	describe(" and the checkbox is clicked",function(){
+		todoList.taskDone(todoList.todos()[0]);
+		it("should mark the task as done", function(){
+			expect(todoList.todos()[0].done()).toEqual(true);
+		});			
+	});
+});
+describe("when an incomplete todo is in the list", function(){
+	var todoList = new TodoListViewModel();	
+	todoList.todos().push(new Task());
+	todoList.todos()[0].name("some task");
+	describe(" and the item is double clicked",function(){
+		todoList.editTask(todoList.todos()[0]);
+		it("should select the edited task", function(){
+			expect(todoList.currentTaskIndex()).toEqual(0);
+		});
+		it("should show the task text for editing", function(){
+			expect(todoList.currentTask().name()).toEqual("some task");
+		});				
+	});
+});
+describe("when an incomplete todo is in the list", function(){
+	var todoList = new TodoListViewModel();	
+	todoList.todos().push(new Task());
+	todoList.todos()[0].name("some task");	
+	describe(" and the item is double clicked",function(){
+		todoList.editTask(todoList.todos()[0]);
+		describe(" and the text is changed", function(){
+			todoList.currentTask().name("some updated task");
+			describe(" and the edit loses focus", function(){
+				todoList.saveTask();
+				it("should update the selected task",function(){
+					expect(todoList.todos()[0].name()).toEqual("some updated task");
+				});
+				it("should clear the selected task",function(){
+					expect(todoList.currentTaskIndex()).toEqual(null);
+				});
+				it("should clear the editable text",function(){
+					expect(todoList.currentTask().name()).toEqual('');
+				});				
+			});
+		});
+	});
+});
+
+describe("when an incomplete todo is in the list", function(){
+	var todoList = new TodoListViewModel();	
+	todoList.todos().push(new Task());
+	todoList.todos()[0].name("some task");	
+	describe(" and the item is double clicked",function(){
+		todoList.editTask(todoList.todos()[0]);
+		describe(" and the text is changed", function(){
+			todoList.currentTask().name("some updated task");
+			describe(" and escape is pressed", function(){
+				todoList.clearCurrentTask();
+				it("should not update the selected task",function(){
+					expect(todoList.todos()[0].name()).toEqual("some task");
+				});
+				it("should clear the selected task",function(){
+					expect(todoList.currentTaskIndex()).toEqual(null);
+				});
+				it("should clear the editable text",function(){
+					expect(todoList.currentTask().name()).toEqual('');
+				});				
+			});
+		});
+	});
+});
+describe("when an incomplete todo is in the list", function(){
+	var todoList = new TodoListViewModel();	
+	todoList.todos().push(new Task());
+	todoList.todos()[0].name("some task");	
+	describe(" and the item is double clicked",function(){
+		todoList.editTask(todoList.todos()[0]);
+		describe(" and the text is changed", function(){
+			todoList.currentTask().name("some updated task");
+			describe(" and the enter is pressed", function(){
+				todoList.saveTask();
+				it("should update the selected task",function(){
+					expect(todoList.todos()[0].name()).toEqual("some updated task");
+				});
+				it("should clear the selected task",function(){
+					expect(todoList.currentTaskIndex()).toEqual(null);
+				});
+				it("should clear the editable text",function(){
+					expect(todoList.currentTask().name()).toEqual('');
+				});				
+			});
+		});
+	});
+});
+describe("when an incomplete todo is in the list", function(){
+	var todoList = new TodoListViewModel();	
+	todoList.todos().push(new Task());
+	todoList.todos()[0].name("some task");	
+	describe(" and the item is double clicked",function(){
+		todoList.editTask(todoList.todos()[0]);
+		describe(" and the text is emptied", function(){
+			todoList.currentTask().name("");
+			describe(" and the enter is pressed", function(){
+				todoList.saveTask();
+				it("should remove the task",function(){
+					expect(todoList.todos().length).toEqual(0);
+				});
+				it("should remove the selected task",function(){
+					expect(todoList.currentTaskIndex()).toEqual(null);
+				});
+				it("should clear the editable text",function(){
+					expect(todoList.currentTask().name()).toEqual("");
+				});
+			});
+		});
+	});
+});
+
+
+describe("when an incomplete todo is in the list", function(){
+	var todoList = new TodoListViewModel();	
+	todoList.todos().push(new Task());
+	todoList.todos()[0].name("some task");	
+	describe("and hovering over the todo", function(){
+		todoList.toggleActive(todoList.todos()[0]);
+		it("shows the remove button", function(){
+			expect(todoList.todos()[0].removeButtonVisible()).toEqual(true);
+		});
+	});
+});
+
---
+++ UI Test/src/todoListViewModel.js
@@ -0,0 +1,91 @@
+
+function Task(){	
+	var self=this;
+	self.done=ko.observable(false);
+	self.name=ko.observable('');
+	self.active=ko.observable(false);
+	self.removeButtonVisible=ko.computed(function(){
+		return(self.active() && !self.removed());
+	});
+	self.removed=ko.observable(false);
+	self.itemClass=ko.computed(function(){
+		if(self.removed()) return "removedItem";
+		if(self.done()) return "doneItem";
+		return "incompleteItem";
+	});
+}
+
+function TodoListViewModel(){
+	var self=this;
+	self.todos = ko.observableArray();
+	self.currentTask=ko.observable(new Task());
+	self.currentTaskIndex=ko.observable(null);	
+	self.leftTotal=ko.computed(function(){
+		var incompleteTasks = 0;
+		for(var t=0;t<self.todos().length;t++){
+			var task = self.todos()[t];
+			if(!task.done() && !task.removed()){
+				incompleteTasks++;
+			}
+		}
+		if(incompleteTasks==0){
+			return "0 items left";
+		}
+		if(incompleteTasks==1){
+			return "1 item left";
+		}
+		if(incompleteTasks>1){
+			return ""+incompleteTasks+" items left";
+		}
+		
+	});
+	
+	self.taskDone = function(task){
+		task.done(true);
+	}
+	self.clearCurrentTask = function(){
+		self.currentTask(new Task());		
+		self.currentTaskIndex(null);			
+	}
+	self.saveTask = function(){
+		self.currentTask().name($.trim(self.currentTask().name()));
+		if(self.currentTaskIndex()=== null){			
+			if(self.currentTask().name()!='')
+			{
+				self.todos.push(self.currentTask());				
+				self.clearCurrentTask();
+			}
+		}else{
+				if(self.currentTask().name()!=''){
+					var taskToUpdate = self.todos()[self.currentTaskIndex()];
+					taskToUpdate.name(self.currentTask().name());				
+				}else{
+					self.todos.remove(self.todos()[self.currentTaskIndex()]);
+				}
+				self.clearCurrentTask();
+		}
+	}
+	self.currentTaskKeyup = function(event,code){
+		if(code.which===27){
+			self.clearCurrentTask();		
+		}
+		if(code.which===13){
+			self.saveTask();
+		}
+		return true;
+		//debugger;
+	};
+	
+	self.toggleActive = function(task){
+		task.active(!task.active());
+		return true;
+	}
+	self.removeTask = function(task){
+		task.removed(true);		
+	}
+	self.editTask = function(task){
+		self.currentTaskIndex(self.todos.indexOf(task));
+		self.currentTask(new Task());
+		self.currentTask().name(task.name());
+	}
+}
